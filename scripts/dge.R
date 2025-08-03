13d466132b532b408a85d78945680b6c6458d2ce
Drzezo55 <awad.abdelaziz.0505@gmail.com>
2025-08-03 04:12:43 +0300

    first  added

--- scripts/dge.R
+++ scripts/dge.R
@@ -2,8 +2,8 @@ library(here)
 here()
 install.packages("renv")
 renv::init()
-load("data/matrix.RData")
-load("data/data.RData")
+load("project_files/data/matrix.RData")
+load("project_files/data/data.RData")
 exp <- expr_df
 metadata <- data
 View(exp)
@@ -17,7 +17,6 @@ install.packages("remotes")
 remotes::install_github("dviraran/xCell")
 install.packages("pheatmap")
 
-library(xCell)
 library(pheatmap)
 library(vsn)
 library(DOSE)
@@ -55,13 +54,109 @@ metadata <- metadata %>%
     survival_status = ifelse(vital_status == "Dead", 1, 0),
     survival_days = days_to_last_follow_up
   )
+
 metadata <- metadata %>%
   mutate (three_year_survival = ifelse(survival_status == 1 & survival_days < 3*365, 1, 0))
+
+
+
+
+
 metadata$three_year_survival
 metadata <- metadata[!is.na(metadata$three_year_survival), ]
+
 summary(metadata$three_year_survival)
 metadata$three_year_survival <- as.factor(metadata$three_year_survival)
+library(gt)
+library(gtsummary)
+library(chromote)
+
+mytable <- metadata %>%
+  mutate(three_year_survival= factor(three_year_survival, levels= c(0,1), labels = c("alive", "dead"))) %>%
+  select(race, gender, ethnicity, prior_malignancy, year_of_diagnosis, age_at_diagnosis, survival_days, three_year_survival ) %>%
+  tbl_summary(
+    by = three_year_survival, missing_text =  "missing"
+  ) %>%
+  as_gt()
+gt::gtsave(mytable, filename = "characteristics.png")
+library(caret)
+library(survival)
+install.packages("survminer")
+library(survminer)
 metadata <- downSample(metadata, metadata$three_year_survival, list = FALSE, yname = "three_year_survival")
+
+surv_obj <- Surv(time = metadata$survival_days, event = metadata$survival_status)
+
+fit <- survfit(surv_obj ~ 1)  # Overall survival
+ggsurvplot(fit,
+           data = metadata,
+           conf.int = TRUE,
+           risk.table = TRUE,
+           title = "Overall Survival",
+           xlab = "Days",
+           ylab = "Survival Probability")
+library(ggplot2)
+
+install.packages("ggpubr")
+library(ggpubr)
+
+# Plot
+table(metadata$three_year_survival, metadata$ethnicity)
+chi_res <- chisq.test(table(metadata$three_year_survival, metadata$gender))
+p_val <- chi_res$p.value
+
+metadata %>%
+  mutate(three_year_survival= factor(three_year_survival, levels= c(0,1), labels = c("alive", "dead"))) %>%
+  ggplot(aes(x = three_year_survival, fill = gender)) +
+  geom_bar(position = "dodge") +
+  labs(
+    title = "3-Year Survival",
+       x = "survival status",
+       y = "gender", 
+      fill = "gender"
+  ) +
+  theme_minimal() +
+  annotate("text", x = 1.5, y = max(table(metadata$three_year_survival)), 
+           label = paste0("Chi-square p = ", signif(p_val, 3)), 
+           size = 5, fontface = "italic")
+
+#
+
+
+library(ggplot2)
+metadata %>%
+  mutate(three_year_survival= factor(three_year_survival, levels= c(0,1), labels = c("alive", "dead"))) %>%  
+  ggplot(aes(x = three_year_survival, y = age_at_diagnosis, fill = three_year_survival)) +
+  geom_violin(trim = FALSE, alpha = 0.7) +
+  geom_boxplot(width = 0.1, fill = "white") +
+  labs(title = "Survival Days by 3-Year Outcome",
+       x = "3-Year Survival",
+       y = "Survival Days") +
+  theme_minimal() +
+  theme(legend.position = "lowerbottom")
+
+metadata %>%
+  mutate(survival_status = factor(survival_status, levels = c(0, 1), labels = c("alive", "dead"))) %>%  
+  ggviolin(x = "gender", y = "survival_days",
+           fill = "survival_status", palette = "jco",
+           add = "boxplot", add.params = list(fill = "white")) +
+  stat_compare_means(aes(group = survival_status), method = "t.test") +
+  labs(
+    title = "Survival Days by Gender and Overall Survival Status",
+    x = "Gender",
+    y = "Survival Days",
+    fill = "Survival Status"
+  ) +
+  theme_minimal()
+
+
+
+
+
+
+
+
+
 metadata$three_year_survival <- NULL
 
 metadata <- metadata %>%
@@ -133,14 +228,31 @@ ggsave("results/volcano_plot.png", width = 6, height = 5, dpi = 300)
 sig_res$gene <- row.names(sig_res)
 sig_res$gene <- str_replace(sig_res$gene, "\\.\\d+$", "")
 genes <- sig_res$gene
-gene_mapping <- bitr(genes, fromType = "ENSEMBL",
-                     toType = "ENTREZID", 
-                     OrgDb = org.Hs.eg.db)
 
+#
+all_genes <- row.names(dds)
+all_genes <- unlist(all_genes)
+all_genes <- str_replace(all_genes, "\\.\\d+$", "")
+
+# Retrieve mappings
+library(biomaRt)
+mart <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+
+all_mapping <- getBM(
+  filters = "ensembl_gene_id",
+  attributes = c("ensembl_gene_id", "entrezgene_id", "external_gene_name"),
+  values = all_genes,
+  mart = mart
+)
+sig_mapping <-  getBM(
+  filters = "ensembl_gene_id",
+  attributes = c("ensembl_gene_id", "entrezgene_id", "external_gene_name"),
+  values = sig_res$gene,
+  mart = mart
+)
 # Merge with results
-sig_mapped <- merge(sig_res, gene_mapping, by.x = "gene", by.y = "ENSEMBL")
 # GO enrihcment 
-ego <- enrichGO(gene = sig_mapped$ENTREZID,
+ego <- enrichGO(gene = sig_mapping$entrezgene_id,
                 OrgDb = org.Hs.eg.db,
                 ont = "BP",   # Biological Process
                 pAdjustMethod = "BH",
@@ -154,5 +266,108 @@ dev.off()
 gene_list <- sig_mapped$log2FoldChange
 names(gene_list) <- sig_mapped$ENTREZID
 # Sort in decreasing order
+kk <- enrichKEGG(gene         = sig_mapping$entrezgene_id,
+                 organism     = 'hsa',
+                 pvalueCutoff = 0.05)
+head(kk)
+library(DOSE)
+library(clusterProfiler)
+x <- enrichDO(gene          = sig_mapping$entrezgene_id,
+              ont           = "HDO",
+              pvalueCutoff  = 0.05,
+              pAdjustMethod = "BH",
+              universe      = as.character(all_mapping$entrezgene_id),
+              minGSSize     = 5,
+              maxGSSize     = 500,
+              qvalueCutoff  = 0.05,
+              readable      = FALSE)
+head(x)
+barplot(x)
+
+genelist <- res$log2FoldChange
+genelist <- unique(genelist)
+names(genelist) <- all_mapping$entrezgene_id
+
+genelist <- sort(genelist, decreasing = TRUE)
+genelist <- genelist[!is.na(names(genelist)) & !duplicated(names(genelist))]
+ego3 <- gseGO(geneList     = genelist,
+              OrgDb        = org.Hs.eg.db,
+              ont          = "CC",
+              minGSSize    = 100,
+              maxGSSize    = 500,
+              pvalueCutoff = 0.05,
+              verbose      = FALSE)
+goplot(ego)
+#
+BiocManager::install("msigdbr")
+library(msigdbr)
+genesets <- msigdbr()
+# Show all pathway names
+sets <- msigdbr(species = "Homo sapiens", category = "C2")
+unique(sets$gs_collection_name)
+# Get KEGG Medicus gene sets for Homo sapiens
+Reactome_Pathways <- msigdbr(species = "Homo sapiens")
+Reactome_Pathways <- subset(Reactome_Pathways, gs_collection_name == "Reactome Pathways")
+term2gene <- Reactome_Pathways[, c("gs_name", "gene_symbol")]
+# Unique gene symbols from your term2gene table
+gene_symbols <- unique(term2gene$gene_symbol)
+
+gene_list <- res$log2FoldChange
+res <- results(dds)
+
+# Extract Ensembl IDs from rownames
+ensembl_ids <- rownames(res) |> str_replace("\\.\\d+$", "")
+
+# Create res_df and attach Ensembl IDs
+res_df <- as.data.frame(res) %>%
+  mutate(ensembl_id = ensembl_ids)
+
+# Join DESeq2 results with Entrez IDs
+res_clean <- res_df %>%
+  inner_join(all_mapping, by = c("ensembl_id" = "ensembl_gene_id"))
 
 
+names(res)
+
+  
+res_clean <- res_clean[!duplicated(res_clean$ensembl_id),]
+
+row.names(res_clean) <- res_clean$ensembl_id
+genes <- res_clean$log2FoldChange
+names(genes) <- row.names(res_clean)
+
+# Map to ENTREZ IDs
+symbol_to_entrez <- getBM(
+  attributes = c("hgnc_symbol", "entrezgene_id"),
+  filters    = "hgnc_symbol",
+  values     = gene_symbols,
+  mart       = mart
+)
+
+# Remove entries without ENTREZ ID
+symbol_to_entrez <- symbol_to_entrez[!is.na(symbol_to_entrez$entrezgene_id), ]
+symbol_to_entrez <- symbol_to_entrez[!duplicated(symbol_to_entrez$hgnc_symbol), ]
+
+term2gene_entrez <- term2gene %>%
+  inner_join(symbol_to_entrez, by = c("gene_symbol" = "hgnc_symbol")) %>%
+  dplyr::select(gs_name, entrezgene_id)
+genelist <- sort(genes, decreasing = TRUE)
+names(genelist) <- res_clean$entrezgene_id
+genelist <- genelist[!is.na(names(genelist)) & !duplicated(names(genelist))]
+gsea_result <- GSEA(
+  geneList    = genelist,   # named numeric vector: names = SYMBOLs, values = logFC/stat
+  TERM2GENE   = term2gene_entrez,
+  pvalueCutoff = 0.01
+)
+length(intersect(names(genelist), term2gene_entrez$entrezgene_id))
+enrichplot::dotplot(gsea_result, showCategory = 20)
+class(gsea_result)
+
+# This works correctly:
+enrichplot::dotplot(gsea_result, showCategory = 20) +
+  theme(
+    axis.text.y = element_text(size = 5),  # Y-axis (pathway) label size
+    axis.text.x = element_text(size = 5),  # X-axis label size
+    axis.title = element_text(size = 5),   # X/Y axis title size
+    plot.title = element_text(size = 5, face = "bold")  # Optional
+  )
